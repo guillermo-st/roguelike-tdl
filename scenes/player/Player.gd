d557a139bc1beda5549bb56eb0b3fc66d49516ac
Simón Stein <86434696+stein257@users.noreply.github.com>
2022-10-28 16:03:26 -0300

    Player first iteration (#7)
    * player first iteration
    
    * enemy has a kinematic body
    
    * added atack animation and resized player colider
    
    * fixed warnings
    
    * added AttackTimer as a child node of weapon

---
+++ scenes/player/Player.gd
@@ -0,0 +1,57 @@
+extends KinematicBody2D
+
+export var max_speed = 500
+export var acceleration = 2000
+var motion = Vector2.ZERO
+var is_attacking = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	var axis = get_input_axis()
+	
+	if axis == Vector2.ZERO && !self.is_attacking:
+		$AnimatedSprite.play("idle")
+		apply_friction(acceleration * delta)
+	else:
+		if !self.is_attacking:
+			$AnimatedSprite.play("run")		
+		if axis.x < 0 and $Weapon.position.x > 0:
+			$AnimatedSprite.flip_h = true
+			$Weapon.flip_horizontally()
+			$Weapon.rotation = -$Weapon.rotation			
+		elif axis.x > 0 and $Weapon.position.x < 0:
+			$AnimatedSprite.flip_h = false
+			$Weapon.flip_horizontally()
+			$Weapon.rotation = -$Weapon.rotation
+		apply_movement(axis * acceleration * delta)
+	motion = move_and_slide(motion)
+
+func get_input_axis():
+	var axis = Vector2.ZERO
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return axis.normalized()
+	
+
+func apply_friction(an_amount_of_friction):
+	if motion.length() > an_amount_of_friction:
+		motion -= motion.normalized() * an_amount_of_friction
+	else:
+		motion = Vector2.ZERO
+
+func apply_movement(an_ammount_of_acceleration):
+	motion += an_ammount_of_acceleration
+	motion = motion.limit_length(max_speed)
+
+
+func _on_Weapon_hit_attempt_started():
+	self.is_attacking = true
+	$AnimatedSprite.play("hit")
+	
+
+
+func _on_Weapon_hit_attempt_ended():
+	self.is_attacking = false
