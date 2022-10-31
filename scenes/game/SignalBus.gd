1d2a56f3e923ac64ebdf61eaf465e38aa336b9f7
guillermo-st <guillermo_stancanelli@outlook.com>
2022-10-31 09:26:11 -0300

    Create the player_entered_level global signal

--- scenes/game/SignalBus.gd
+++ scenes/game/SignalBus.gd
@@ -1,10 +1,7 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+signal player_entered_level(level_pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
