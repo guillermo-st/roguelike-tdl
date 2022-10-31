c49ae8ebcddd83ac07b7c2a20a0dc6e3f0f8847a
guillermo-st <guillermo_stancanelli@outlook.com>
2022-10-31 09:35:52 -0300

    Add a Game and Camera controllers

---
+++ scenes/game/CameraController.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SignalBus.connect("player_entered_level", self, "move_camera")
+
+
+
+func move_camera(target_position):
+	$Tween.interpolate_property($Camera2D, "global_position", $Camera2D.global_position, target_position, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
