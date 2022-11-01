4b1262e450fb0cb8e2ce031d00660eef0e93a51e
Guillermo Stancanelli <66892180+guillermo-st@users.noreply.github.com>
2022-10-31 23:36:58 -0300

    9 Game Camera (#12)
    * remove the camera from the LevelGenerator
    
    * Modify BaseLevel to support dynamic camera
    
    * singleton for 'long distance' signals
    
    * Create the player_entered_level global signal
    
    * baseLevel now emits player_entered signal
    
    * Add a Game and Camera controllers
    
    * Change the main scene

--- scenes/game/SignalBus.gd
+++ scenes/game/SignalBus.gd
@@ -1,13 +1,13 @@
-extends Node2D
+extends Node
+
+
+signal player_entered_level(level_pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Area2D_body_entered(_body):
-	SignalBus.emit_signal("player_entered_level", $CenterAnchor.global_position)
