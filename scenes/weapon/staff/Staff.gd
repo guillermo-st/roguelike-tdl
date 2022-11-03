dd0d82b4dc54d0566b25ead7ea3cfeeaecb2b12c
Simon Stein <sstein@fi.uba.ar>
2022-11-03 01:10:26 -0300

    added fireball logic to staff

--- scenes/weapon/staff/Staff.gd
+++ scenes/weapon/staff/Staff.gd
@@ -1,8 +1,9 @@
 extends "res://scenes/weapon/Weapon.gd"
 
-var weapon_position = Vector2(-35, 10)
+var weapon_position = Vector2(-35, 0)
 var weapon_scale = Vector2(3, 3)
-var weapon_rotation = 90
+var weapon_rotation = -270
+var particles = preload("res://scenes/weapon/staff/fireball/FireBall.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,11 +15,18 @@ func _ready():
 
 func behave_when_hitting():
 	self.elpased_hit_time = 0
+	self.shoot_fireball()
 	emit_signal("hit_attempt_started")
 	$Pivot/AnimationPlayer.play("Attack")
 	$AttackTimer.start()
-
-func _on_AttackTimer_timeout():
-	emit_signal("hit_attempt_ended")
-	$Pivot/AnimationPlayer.play("Re")
 	
+
+func _on_AttackTimer_timeout():	
+	emit_signal("hit_attempt_ended")
+	
+func shoot_fireball():
+	var fireball = particles.instance()
+	fireball.rotation_direction = self.get_parent().rotation
+	fireball.position = $ShootSpawner.get_global_position()
+	fireball.rotation = self.get_parent().rotation + PI / 2
+	get_tree().get_root().add_child(fireball)
