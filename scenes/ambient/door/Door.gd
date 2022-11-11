80390c696ab07705d614b042a85d47f9235dae31
guillermo-st <guillermo_stancanelli@outlook.com>
2022-11-11 14:17:18 -0300

    Add a lock and sealing ability to door

--- scenes/ambient/door/Door.gd
+++ scenes/ambient/door/Door.gd
@@ -4,16 +4,19 @@ signal player_entered_door
 
 var should_position_player
 var is_open
+var is_sealed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	should_position_player = true
 	$Sprite.modulate.a = 0
 	is_open = true
+	is_sealed = false
+
 
 func close():
-	if is_open:
-		#$Area2D/CollisionShape2D.set_deferred("disabled", false)
+	if is_open and not is_sealed:
+		$DoorLock.global_position = self.global_position
 		$Tween.interpolate_property($Sprite, "global_position", $OpenPosition.global_position, self.global_position, 1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
 		$Tween.interpolate_property($Sprite, "modulate", Color(1,1,1,0), Color(1,1,1,1), 1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT )
 		$Tween.start()
@@ -21,15 +24,21 @@ func close():
 		is_open = false
 
 func open():
-	#$Area2D/CollisionShape2D.set_deferred("disabled", true)
-	$Tween.interpolate_property($Sprite, "global_position", self.global_position, $OpenPosition.global_position, 1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
-	$Tween.interpolate_property($Sprite, "modulate", Color(1,1,1,1), Color(1,1,1,0), 1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT )
-	$Tween.start()
-	is_open = true
+	if not is_open and not is_sealed:
+		$DoorLock.global_position = $OpenPosition.position
+		$Tween.interpolate_property($Sprite, "global_position", self.global_position, $OpenPosition.global_position, 1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
+		$Tween.interpolate_property($Sprite, "modulate", Color(1,1,1,1), Color(1,1,1,0), 1, Tween.TRANS_EXPO, Tween.EASE_IN_OUT )
+		$Tween.start()
+		is_open = true
 
+func seal():
+	is_sealed = true
+	$Sprite.modulate = Color(0.3, 0.3, 0.3, 1)
+	$Sprite.global_position = self.global_position
+	$DoorLock.global_position = self.global_position
+	$EnterArea.monitoring = false
 
 func _on_EnterArea_body_entered(body):
 	if should_position_player:
 		body.global_position = $EnterPosition.global_position
-	$Area2D/CollisionShape2D.disabled = false
 	emit_signal("player_entered_door")
