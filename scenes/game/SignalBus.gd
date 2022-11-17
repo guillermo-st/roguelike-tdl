5824d496efada563215fb1223ea7a4ec50979814
guillermo-st <guillermo_stancanelli@outlook.com>
2022-11-17 17:28:32 -0300

    New Bus signal for changing zones

--- scenes/game/SignalBus.gd
+++ scenes/game/SignalBus.gd
@@ -2,12 +2,4 @@ extends Node
 
 
 signal camera_interest_shifted(level_pos)
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+signal zone_exited()
