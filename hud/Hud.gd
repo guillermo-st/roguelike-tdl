ac2ae74bd9da4ee1db32a0d71ce4b7debbe77ac8
Simon Stein <sstein@fi.uba.ar>
2022-11-18 17:07:40 -0300

    add player heal signal connection

--- hud/Hud.gd
+++ hud/Hud.gd
@@ -4,6 +4,12 @@ onready var life_bar = $LifeBar
 
 func on_player_hit(damage):
 	life_bar.life -= damage
+	
+
+func on_player_heal(heal_amount):
+	if not (life_bar.life + heal_amount > life_bar.max_life):
+		life_bar.life += heal_amount
+
 
 func revive():
 	life_bar.reset()
