ee52677fb4c72d65ad8704e91dfcff3a50be8df3
guillermo-st <guillermo_stancanelli@outlook.com>
2022-11-20 16:41:07 -0300

    fix Exit bug

--- scenes/ambient/exit/Exit.gd
+++ scenes/ambient/exit/Exit.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 onready var sprite = $AnimatedSprite
-onready var lock = $Lock
 
 
 func _ready():
@@ -9,7 +8,9 @@ func _ready():
 
 func open():
 	sprite.play("open")
-	lock.queue_free()
+	var lock = get_node_or_null("Lock")
+	if lock:
+		lock.queue_free()
 
 func _on_Exit_body_entered(_body):
 	SignalBus.emit_signal("zone_exited")
