b6527488324811f2b6da50884fb23a880141a791
Simon Stein <sstein@fi.uba.ar>
2022-11-20 17:32:37 -0300

    change hud player heal, if heal ammount overflows health player gets max life

--- hud/Hud.gd
+++ hud/Hud.gd
@@ -9,6 +9,8 @@ func on_player_hit(damage):
 func on_player_heal(heal_amount):
 	if not (life_bar.life + heal_amount > life_bar.max_life):
 		life_bar.life += heal_amount
+	else:
+		life_bar.life = life_bar.max_life
 
 
 func revive():
