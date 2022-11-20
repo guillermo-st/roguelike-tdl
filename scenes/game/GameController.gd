177d017cec6def9b7068c9486c3e095311b0b64c
guillermo-st <guillermo_stancanelli@outlook.com>
2022-11-20 16:29:25 -0300

    Support multiple zone with level generation

--- scenes/game/GameController.gd
+++ scenes/game/GameController.gd
@@ -1,18 +1,23 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var current_zone
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Player.global_position = $LevelGenerator/InitialLevel/baseLevel/CenterAnchor.global_position
 	$Player.connect("take_damage", $Hud, "on_player_hit")
+	SignalBus.connect("zone_exited", self, "start_new_zone_deferred")
+	start_new_zone()
+
+func start_new_zone_deferred():
+	call_deferred("start_new_zone")
+
+func start_new_zone():
+	if current_zone != null:
+		remove_child(current_zone)
+		current_zone.queue_free()
+	
+	var zone_and_pos = $LevelGenerator.get_next_zone()
+	current_zone = zone_and_pos[0]
+	add_child(current_zone)
+	$Player.global_position = zone_and_pos[1]
 	$CameraController/Camera2D.global_position = $Player.global_position
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
